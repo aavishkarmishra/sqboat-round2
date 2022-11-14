d567684bf2e84651e1d6abec5323edcf7ca235a2
aavishkarmishra <aavishakrmishra@gmail.com>
2022-11-14 06:44:12 +0530

    Integrated client and server

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const authRoutes = require("./routes/api/auth");
 const userRoutes = require("./routes/api/users");
 const productRoutes = require("./routes/api/products");
 const orderRoutes = require("./routes/api/orders");
-const routes = require("./routes/routes");
 const connectDB = require("./utils/configDB");
+
 const PORT = process.env.PORT || 5000;
 
 const app = express();
@@ -26,12 +26,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
-
-app.use(express.static("client/build"));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname,  "client", "build", "index.html"));
-});
-
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 app.listen(PORT, () =>
   console.log(`Server listening on http://localhost:${PORT}/api`),
 );
