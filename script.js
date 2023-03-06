b138e60110cd2a319cf520aa8d943ddb83f72227
Aavishkar Mishra <aavishkarmishra@gmail.com>
2023-03-07 03:47:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
-const onEmailLoad = () => {
-  console.log('Email is loaded!');
+const https = require('https');
+const url = require('url');
+
+const query = url.parse(req.url, true).query;
+
+
+const options = {
+  hostname: 'devapi1.joinscream.com',
+  port: 443,
+  path: `/api/v1/notification/email/open/${query.campaignId}`,
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  }
 };
 
-const onEmailUnsubscribe = (event) => {
-  console.log(event.target);
-  const creatorEmail = event.target.getAttribute('data-sender');
-  const consumerEmail = event.target.getAttribute('data-receiver');
-  console.log(creatorEmail, consumerEmail);
+const data = {
+  email: 'recipient-email-address@example.com',
+  timestamp: new Date().toISOString()
 };
+
+const req = https.request(options, (res) => {
+  console.log(`API request sent: ${res.statusCode} ${res.statusMessage}`);
+});
+
+req.on('error', (error) => {
+  console.error('API request error:', error);
+});
+
+req.write(JSON.stringify(data));
+req.end();
